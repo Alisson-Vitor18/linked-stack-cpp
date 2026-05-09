922ae8ba30474bf8fde6ce22a6017f4928e527ed
Alisson-Vitor18 <alissonvpb18@gmail.com>
2026-05-09 14:46:53 -0300

    Adicionando std::stack na main para fins de comparação

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <stdexcept>
+#include <stack>
 #include <memory>
 #include "stack.h"
+
+void print_stack_cpp(std::stack<int>& stack_cpp);
 
 int main() {
     Stack<int> stack;
@@ -15,5 +17,28 @@ int main() {
     stack.print(std::cout); 
     std::cout << std::endl;
 
+    std::cout << std::endl;
+    std::stack<int> stack_cpp;
+    stack_cpp.push(1);
+    stack_cpp.push(2);
+    stack_cpp.push(3);
+    stack_cpp.push(4);
+    stack_cpp.push(5);
+
+    print_stack_cpp(stack_cpp);
+    std::cout << std::endl;
+    
     return 0;
+}
+
+void print_stack_cpp(std::stack<int>& stack_cpp) {
+    bool first = true;
+    std::cout << "[";
+    while (!stack_cpp.empty()) {
+        if(!first) std::cout << ", ";
+        std::cout << stack_cpp.top();
+        stack_cpp.pop();
+        first = false;
+    }
+    std::cout << "]";
 }
